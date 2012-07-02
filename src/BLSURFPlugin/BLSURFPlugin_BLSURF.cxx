12d4b0683200993cfbf1b15202d5951912be568f
gdd <gdd>
2012-07-02 12:21:08 +0000

    'struct SMESH_TNodeXYZ' has no member named '_xyz'

--- src/BLSURFPlugin/BLSURFPlugin_BLSURF.cxx
+++ src/BLSURFPlugin/BLSURFPlugin_BLSURF.cxx
@@ -1492,7 +1492,8 @@ bool BLSURFPlugin_BLSURF::Compute(SMESH_Mesh& aMesh, const TopoDS_Shape& aShape)
           double t                = nData.param;
           real uv[2]              = { nData.u, nData.v };
           SMESH_TNodeXYZ nXYZ( nData.node );
-          dcad_edge_discretization_set_vertex_coordinates( dedge, iN+1, t, uv, nXYZ._xyz );
+          real xyz[3]             = { nXYZ.X(), nXYZ.Y(), nXYZ.Z() };
+          dcad_edge_discretization_set_vertex_coordinates( dedge, iN+1, t, uv, xyz );
         }
         dcad_edge_discretization_set_property(dedge, DISTENE_DCAD_PROPERTY_REQUIRED);
       }

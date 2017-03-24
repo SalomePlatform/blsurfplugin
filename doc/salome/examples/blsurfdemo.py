cc9bebfae00f1dac7c3d45be49b02e7011c54863
Gilles DAVID <gilles-g.david@edf.fr>
2017-03-24 10:27:15 +0100

    Python3: doc example

--- doc/salome/examples/blsurfdemo.py
+++ doc/salome/examples/blsurfdemo.py
@@ -72,8 +72,8 @@ algo2d.SetEnforcedVertex(Face_1, 150, 150, 150)
 
 # Retrieve and print the list of enforced vertices defines on Face_1
 enfList = algo2d.GetEnforcedVertices(Face_1)
-print "List of enforced vertices for Face_1: "
-print enfList
+print("List of enforced vertices for Face_1: ")
+print(enfList)
 
 # compute the mesh
 cadsurfMesh.Compute()
@@ -81,8 +81,8 @@ cadsurfMesh.Compute()
 # Remove an enforced vertex and print the list
 algo2d.UnsetEnforcedVertex(Face_1, 50, 50, 50)
 enfList = algo2d.GetEnforcedVertices(Face_1)
-print "List of enforced vertices for Face_1: "
-print enfList
+print("List of enforced vertices for Face_1: ")
+print(enfList)
 
 # compute the mesh
 cadsurfMesh.Compute()
